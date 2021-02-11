8bc560671d4d5e181779612cfa80d67f6b2e3917
Konstantin V. Borisov <smeagol74@gmail.com>
2021-02-11 20:35:12 +0700

    * fixed Calibre call

--- ol-calibre.el
+++ ol-calibre.el
@@ -8,15 +8,14 @@
                          :export #'org-calibre-export
                          )
 
-(defcustom org-calibre-command 'calibre
+(defcustom org-calibre-command "calibre"
   "The Emacs command to be used to display a calibre query."
   :group 'org-link
-  :type '(string))
+  :type '(string)) 
 
-(defun org-calibre-open (path _)
-  "Visit the manpage on PATH.
-PATH should be a topic that can be thrown at the man command."
-  (funcall org-calibre-command path))
+(defun org-calibre-open (query)
+  "QUERY should be a string querying in Calibre search."
+  (async-start-process "Calibre" org-calibre-command nil query))
 
 (defun org-calibre-export (link description format _)
   "Export a calibre page link from Org files."
